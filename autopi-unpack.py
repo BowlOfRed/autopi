372604304e66d7b3729e65a7f14057f1ad2e709c
BowlOfRed <bigdnospam-github@yahoo.com>
2017-10-02 23:29:05 -0700

    First file checks

--- autopi-unpack.py
+++ autopi-unpack.py
@@ -1,9 +1,15 @@
-#!/bin/python
+#!/usr/bin/python3
 import sys
+import os
 
 #Given an image name and a directory, unpacks the raspbian image
 #into the directory
 
 if (len(sys.argv) != 3):
-  sys.exit("Usage: %s [image] [respository]")
+  sys.exit("Usage: %s [image] [respository]" % sys.argv[0])
 
+image,repository = sys.argv[1:3]
+print("image: %s repo: %s" % (image,repository))
+
+if (not os.path.exists(image)):
+  sys.exit("%s: Image path %s does not exist." % (sys.argv[0], image))
